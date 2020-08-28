f2be33df27caa42493e49672f8c3e25469dd2475
Sergio <sergio_siccha_12@hotmail.com>
2020-08-28 18:33:26 -0500

    GitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CamaraComponent } from './camara/camara.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CamaraComponent
   ],
   imports: [
     BrowserModule,
